ad42ee6f59d4a3e26376cd326168c19dc783d12e
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-16 22:02:00 -0600

    commit tree 123704017318e95a63fcf3760b8984634b6ce6b2

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "Brown", "Excitable", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 15, "M", "Tan", "Sassy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "White", "Reserved", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "F", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 12, "M", "Light Brown", "Moody", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "Green", "Pessimistic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 13, "F", "Black", "Outgoing", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, "F", "Black", "Rigid", 0);
